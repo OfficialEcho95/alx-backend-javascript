9000f71f4c32f5ecc07623e15f7188a26f4e23d7
Emmanuel Chukwu <officialecho95@outlook.com>
2023-07-04 17:02:25 +0100

    typed arrays

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -2,8 +2,8 @@ export default function createInt8TypedArray(length, position, value) {
   const uint8 = new Uint8Array(3);
   const buffer = new ArrayBuffer(length);
 
-  if (position < 0 || position >= length){
-    throw new Error("Position outside range");
+  if (position < 0 || position >= length) {
+    throw new Error('Position outside range');
   }
 
   uint8[position] = value;
