9b9b0c44e55e4e23ffbfb917930771c138342c84
Emmanuel Chukwu <officialecho95@outlook.com>
2023-07-04 17:45:45 +0100

    start string

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,11 +2,12 @@ export default function cleanSet(set, startString) {
   let result = '';
   for (const elem of set) {
     if (elem.startsWith(startString)) {
-      result += `${elem.substring(startString.length)}-`;
+      result += elem.substring(startString.length) + "-";
     }
   }
   if (startString === '') {
     return '';
   }
+  result = result.slice(0, -1);
   return result;
 }
