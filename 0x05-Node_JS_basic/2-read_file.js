a334c535f741bd0d09d8cb55313632beb1dbafcf
Emmanuel Chukwu <officialecho95@outlook.com>
2023-08-22 07:04:36 +0100

    reading from database

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,9 +1,36 @@
 const fs = require('fs');
 
 function countStudents(filePath) {
-    try {
-        const data = fs.readFileSync(filePath, 'utf8');
+  try {
+    const data = fs.readFileSync(filePath, 'utf8');
+    // Filter out empty lines
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+    const students = lines.slice(1).map((line) => line.split(','));
 
-        if (data in )
+    const fields = {};
+    for (const student of students) {
+      const field = student[3];
+      if (!fields[field]) {
+        fields[field] = [];
+      }
+      fields[field].push(student[0]);
     }
+
+    console.log(`Number of students: ${students.length}`);
+    for (const field in fields) {
+      if (Object.prototype.hasOwnProperty.call(fields, field)) {
+        console.log(
+          `Number of students in ${field}: ${fields[field].length} `
+          + `. List: ${fields[field].join(', ')}`,
+        );
+      }
+    }
+  } catch (err) {
+    console.error('Cannot load the database');
+  }
 }
+
+module.exports = countStudents;
+
+const databasePath = 'database.csv';
+countStudents(databasePath);
