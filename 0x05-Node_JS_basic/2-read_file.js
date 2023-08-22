405d4228d1dfa190b059fb02fc140370a46403fb
Emmanuel Chukwu <officialecho95@outlook.com>
2023-08-22 07:34:57 +0100

    corrected output format

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -9,19 +9,19 @@ function countStudents(filePath) {
 
     const fields = {};
     for (const student of students) {
-      const field = student[3];
-      if (!fields[field]) {
-        fields[field] = [];
+        const field = student[3].trim();
+        if (!fields[field]) {
+          fields[field] = [];
+        }
+        fields[field].push(student[0]);
       }
-      fields[field].push(student[0]);
-    }
 
     console.log(`Number of students: ${students.length}`);
     for (const field in fields) {
       if (Object.prototype.hasOwnProperty.call(fields, field)) {
         console.log(
-          `Number of students in ${field}: ${fields[field].length} `
-          + `. List: ${fields[field].join(', ')}`,
+          `Number of students in ${field}: ${fields[field].length}.`
+          + `. List: ${fields[field].join(', ')}`
         );
       }
     }
